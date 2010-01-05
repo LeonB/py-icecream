32439c6de903abfa0bf2f85686978a4a32d35be4
LeonB <leon@tim-online.nl>
2010-01-05 01:05:24 +0100

    Been thinking of a new hook api

--- src/bin/icecream.py
+++ src/bin/icecream.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 ### Twisted Preamble
 # This makes sure that users don't have to set up their environment
 # specially in order to run these programs from bin/.
@@ -14,20 +13,6 @@ sys.path.append(bin_dir + os.sep + os.pardir + os.sep + 'libs' + os.sep)
 import gobject
 gobject.threads_init()
 
-# lines = sys.stdin.readlines()
-
-# for line in lines:
-#         sys.stdout.write(line)
-
-#from server import Server
-#import sources
-
-#script = sources.Script()
-#script.location = ''
-
-#s = Server(source)
-#s.password = 'hackme'
-
 import pyicecream
 from pyicecream import sources
 #from pyicecream.stream import Stream
@@ -35,9 +20,15 @@ from pyicecream import sources
 #This should be wrapped in App, which should also do the configuration part
 s = pyicecream.Stream()
 s.server.host = 'basil'
+s.server.port = 9000
 s.server.password = 'hackmePbq11Kz'
 s.source = sources.Disk('~/Workspaces/py-icecream/samples/gapless')
 s.source = sources.Script('~/Workspaces/py-icecream/src/test/scripttest.py')
+s.source = sources.Script('wget -qO- http://basil/playlist | cat')
+
+s.hooks.source.eof = hooks.Script('')
+s.hooks.source.start_play = hooks.Script('')
+
 s.run()
 
 # enter into a mainloop
---
+++ src/pyicecream/hook.py
@@ -0,0 +1,14 @@
+class Hook(object):
+    """Can be anything: file, script, stdin, et cetera"""
+    """It's meant to be extended, not used directly"""
+
+    #@TODO: how should the input of ogg files be handled?
+            #because they don't have to be de-coded
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        #maybe call the childclass method different
+        #and check here for correct output (http://, file://, et cetera)
+        raise Exception('NIY', 'function run() is not yet implemented!')
